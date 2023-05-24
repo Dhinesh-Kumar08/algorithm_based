07cf4cd2369ac546579081266fb223842a14c742
DHINESH kUMAR.S <dhineshsurya0829@gmail.com>
2023-05-24 20:38:11 +0530

    first commit

---
+++ adding_of _two_number.py
@@ -0,0 +1,6 @@
+def adding_of_twonumber(a,b):
+        return a+b
+num1 = int(input())
+num2 = int(input())
+res = adding_of_twonumber(num1,num2)
+print(res)
---
+++ simple_add_array.py
@@ -0,0 +1,15 @@
+'''but is my logic'''
+arr = []
+n = int(input())
+for i in range(0,n):
+    ins = int(input())
+
+    arr.append(ins)
+print(arr)
+adding = sum(arr)
+print(adding)
+'''x=0
+    for i in range(0,ar_count):
+        x = x + ar[i]
+    return x'''
+'''this correct answer'''
